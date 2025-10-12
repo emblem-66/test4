68ba8221e6c82cc863f099a008d2eda29cfa005c
emblem-66 <143352594+emblem-66@users.noreply.github.com>
2025-10-12 10:13:43 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 set -xeuo pipefail
 
-
 rpm -qa |sort
 
-#rpm -qa |sort
+rpm -qa 'qemu-user-static*' | xargs dnf remove -y
+
+rpm -qa '*backgrounds*' | xargs dnf remove -y
+
+dnf remove -y sway
+
+rpm -qa |sort
