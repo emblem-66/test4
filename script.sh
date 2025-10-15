199ec263ffa0ccd75ee4aa66107867272d4140c1
emblem-66 <143352594+emblem-66@users.noreply.github.com>
2025-10-15 23:10:47 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 set -xeuo pipefail
 
-jq -r .packages[] /usr/share/rpm-ostree/treefile.json > /usr/local/share/kde-bootc/packages-fedora-bootc
+jq -r .packages[] /usr/share/rpm-ostree/treefile.json > /usr/local/share/packages
 
 rpm -qa | sort
 
 #rpm -qa 'qemu-user-static*' | xargs dnf remove -y
 
-#rpm -qa | sort
